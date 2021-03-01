bac587593e32589496c23f4f8469d3d51adb7d2e
Tyler Shipe <tjshipe@gmail.com>
2021-03-01 15:16:25 -0500

    add background and tweak styling

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,27 +1,25 @@
-import { Image, Menu, Spacer } from 'antd';
-import Logo from './Logo';
+import { Image, Menu } from "antd";
+import Logo from "./Logo";
 
 export default function Navigation() {
   return (
     <>
-      <Menu mode="horizontal" selectedKeys={[]}>
+      <Menu
+        mode="horizontal"
+        selectedKeys={[]}
+        style={{ background: "none", borderBottom: "none" }}
+      >
         <Logo />
         <Menu.Item key="trade">
-          <a href="/trade">
-            Trade
-          </a>
+          <a href="/trade">Trade</a>
         </Menu.Item>
         <Menu.Item key="stats">
-          <a href="/stats">
-            Stats
-          </a>
+          <a href="/stats">Stats</a>
         </Menu.Item>
         <Menu.Item key="help">
-          <a href="/help">
-            Help
-          </a>
+          <a href="/help">Help</a>
         </Menu.Item>
       </Menu>
     </>
   );
-};
+}
