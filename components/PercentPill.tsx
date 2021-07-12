090a880fb6d05b6b1604b2db267ee5f87036606a
Steven Sarmiento <sarmiento.steven@gmail.com>
2021-07-11 21:26:34 -0700

    Adding Home page components for landing page

---
+++ components/PercentPill.tsx
@@ -0,0 +1,9 @@
+const MangoPill = () => {
+  return (
+    <div className="inline-flex items-center px-2 py-1 text-lg text-white text-xs uppercase font-bold bg-red-500 rounded-full">
+      <p>-3%</p>
+    </div>
+  )
+}
+
+export default MangoPill
