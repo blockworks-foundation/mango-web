870ac48a635f10f1bfbedd2a2b1defe2a4c81b10
Steven Sarmiento <sarmiento.steven@gmail.com>
2021-07-12 04:49:39 -0700

    added new components

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,9 @@
 import NavBarBeta from '../components/NavBarBeta'
 import HeroSectionHome from '../components/HeroSectionHome'
-import HomeSection from '../components/HomeSection'
+import TradeSection from '../components/TradeSection'
 import FooterSection from '../components/FooterSection'
+import LendSection from '../components/LendSection'
+import CommunitySection from '../components/CommunitySection'
 
 const Index = () => {
   return (
@@ -9,7 +11,9 @@ const Index = () => {
       <div className="w-screen h-2 bg-gradient-to-r from-red-400 via-yellow-300 to-green-300"></div>
       <NavBarBeta />
       <HeroSectionHome />
-      <HomeSection />
+      <TradeSection />
+      <LendSection />
+      <CommunitySection />
       <FooterSection />
       <div className="w-screen h-2 bg-gradient-to-r from-red-400 via-yellow-300 to-green-300"></div>
     </div>
