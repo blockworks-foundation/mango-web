973323e2d475364167db8c35e7fc20b973108089
Steven Sarmiento <sarmiento.steven@gmail.com>
2021-07-14 15:00:31 -0700

    added items to tailwind.congif

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,28 @@ module.exports = {
     fontFamily: {
       sans: ['Lato', 'sans-serif'],
     },
+
+        
+    screens: {
+      
+      'xs': '240px',
+      // => @media (min-width: 340px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+
     extend: {
       backgroundImage: (_) => ({
         'hero-img': "url('../public/img/Hero_BG.jpeg')",
