0ee3f5a887fc487f7dda7f660536b6036176bbf8
Steven Sarmiento <sarmiento.steven@gmail.com>
2021-07-18 17:01:53 -0700

    cleaning up repo from token launch page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
-import HeroSectionLP from '../components/HeroSectionLP'
-import LandingContent from '../components/LandingContent'
-import FooterSection from '../components/FooterSection'
 import NavBarBeta from '../components/NavBarBeta'
+import HeroSectionHome from '../components/HeroSectionHome'
+import TradeSection from '../components/TradeSection'
+import FooterSection from '../components/FooterSection'
+import LendSection from '../components/LendSection'
+import CommunitySection from '../components/CommunitySection'
 
 const Index = () => {
   return (
     <div className={`bg-th-bkg-1 text-th-fgd-1 transition-all overflow-hidden`}>
       <div className="w-screen h-2 bg-gradient-to-r from-mango-red via-mango-yellow to-mango-green"></div>
       <NavBarBeta />
-      <HeroSectionLP />
-      <LandingContent />
+      <HeroSectionHome />
+      <TradeSection />
+      <LendSection />
+      <CommunitySection />
       <FooterSection />
       <div className="w-screen h-2 bg-gradient-to-r from-mango-red via-mango-yellow to-mango-green"></div>
     </div>
