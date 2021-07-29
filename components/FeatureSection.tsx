c1811509c9674fa523c736481095c04fe8333911
steven <sarmiento.steven@gmail.com>
2021-07-28 21:33:36 -0700

    margin fix for a menu bug

--- components/FeatureSection.tsx
+++ components/FeatureSection.tsx
@@ -285,7 +285,7 @@ const FeatureSection = () => {
         <section className="">
           <div className="grid grid-cols-3 gap-6 mb-6">
             <div className="lg:col-span-1 md:col-span-3 sm:col-span-3 xs:col-span-3">
-              <div className="bg-th-fgd-4 bg-product-one bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
+              <div className="bg-th-fgd-4 border border-bkg-3 bg-product-one bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
                 <div className="py-4 px-8 mt-3">
                   <div className="flex flex-col mb-8">
                     <h2 className="text-yellow-300 font-semibold text-xl tracking-wide mb-2">
@@ -300,7 +300,7 @@ const FeatureSection = () => {
               </div>
             </div>
             <div className="lg:col-span-2 md:col-span-3 sm:col-span-3 xs:col-span-3">
-              <div className="bg-th-fgd-4 bg-product-two bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
+              <div className="bg-th-fgd-4 border border-bkg-3 bg-product-two bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
                 <div className="py-4 px-8 mt-3">
                   <div className="flex flex-col mb-8">
                     <h2 className="text-yellow-300 font-semibold text-xl tracking-wide mb-2">
@@ -318,7 +318,7 @@ const FeatureSection = () => {
 
           <div className="grid grid-cols-3 gap-6">
             <div className="lg:col-span-2 md:col-span-3 sm:col-span-3 xs:col-span-3">
-              <div className="bg-th-fgd-4 bg-product-three bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
+              <div className="bg-th-fgd-4 border border-bkg-3 bg-product-three bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
                 <div className="py-4 px-8 mt-3">
                   <div className="flex flex-col mb-8">
                     <h2 className="text-yellow-300 font-semibold text-xl tracking-wide mb-2">
@@ -333,7 +333,7 @@ const FeatureSection = () => {
               </div>
             </div>
             <div className="lg:col-span-1 md:col-span-3 sm:col-span-3 xs:col-span-3">
-              <div className="bg-th-fgd-4 bg-product-four bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
+              <div className="bg-th-fgd-4 border border-bkg-3 bg-product-four bg-cover bg-no-repeat h-650 w-full shadow-md rounded-xl overflow-hidden mx-auto">
                 <div className="py-4 px-8 mt-3">
                   <div className="flex flex-col mb-8">
                     <h2 className="text-yellow-300 font-semibold text-xl tracking-wide mb-2">
@@ -350,7 +350,7 @@ const FeatureSection = () => {
           </div>
         </section>
 
-        <div className="bg-gradient-to-br from-th-fgd-4 to-th-fgd-4 rounded-xl shadow-md overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4 mt-8">
+        <div className="bg-gradient-to-br from-th-fgd-4 to-th-fgd-4 border border-bkg-3 rounded-xl shadow-md overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4 mt-8">
           <div className="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
             <div className="lg:self-center">
               <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
--- components/NavBarBeta.tsx
+++ components/NavBarBeta.tsx
@@ -110,7 +110,7 @@ const NavBarBeta = () => {
                     !productMenuVisible && 'hidden'
                   } absolute z-50 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2`}
                 >
-                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="rounded-lg border border-bkg-3 shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                     <div className="relative grid gap-6 bg-th-bkg-3 px-5 py-6 sm:gap-8 sm:p-8">
                       <h3 className="text-sm tracking-wide font-medium text-yellow-300 uppercase">
                         Trading
@@ -311,7 +311,7 @@ const NavBarBeta = () => {
                     !supportMenuVisible && 'hidden'
                   } absolute z-50 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0`}
                 >
-                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="rounded-lg border border-bkg-3 shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                     <div className="relative grid gap-6 bg-th-bkg-3 px-5 py-6 sm:gap-8 sm:p-8">
                       <h3 className="text-sm tracking-wide font-medium text-yellow-300 uppercase">
                         Social
@@ -371,7 +371,7 @@ const NavBarBeta = () => {
                         </div>
                       </a>
                     </div>
-                    <div className="px-5 py-5 bg-th-fgd-4 sm:px-8 sm:py-8">
+                    <div className="px-5 py-5 bg-th-fgd-4 -mt-1 sm:px-8 sm:py-8">
                       <div>
                         <h3 className="text-sm tracking-wide font-bold text-yellow-300 uppercase">
                           Mango Guides
