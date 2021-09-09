1ded0273b7e53f0bd2bebbb9ead544c2172e138a
steven <sarmiento.steven@gmail.com>
2021-09-09 00:20:12 -0700

    Styling 404 page

--- components/FourOhFour.tsx
+++ components/FourOhFour.tsx
@@ -5,6 +5,7 @@ import {
   RssIcon,
   ViewListIcon,
 } from '@heroicons/react/outline'
+import GradientText from './GradientText'
 
 const links = [
   {
@@ -94,34 +95,34 @@ const social = [
 
 const FourOhfour = () => {
   return (
-    <div className="bg-white">
+    <div className="bg-bg-texture bg-cover bg-bottom bg-no-repeat">
       <main className="max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex-shrink-0 pt-16">
           <img
             className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
+            src="../img/logo_mango.svg"
             alt="Workflow"
           />
         </div>
         <div className="max-w-xl mx-auto py-16 sm:py-24">
           <div className="text-center">
-            <p className="text-sm font-semibold text-indigo-600 uppercase tracking-wide">
-              404 error
+            <p className="text-sm font-semibold uppercase tracking-wide">
+              <GradientText>404 error</GradientText>
             </p>
-            <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
-              This page does not exist.
+            <h1 className="mt-2 text-4xl font-extrabold text-white tracking-tight sm:text-5xl">
+              Hmm... you must be lost!
             </h1>
             <p className="mt-2 text-lg text-gray-500">
               The page you are looking for could not be found.
             </p>
           </div>
           <div className="mt-12">
-            <h2 className="text-sm font-semibold text-gray-500 tracking-wide uppercase">
+            <h2 className="text-sm font-semibold text-white text-opacity-50 tracking-wide uppercase">
               Popular pages
             </h2>
             <ul
               role="list"
-              className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200"
+              className="mt-4 border-t border-b border-white divide-y divide-white divide-opacity-20 border-opacity-20"
             >
               {links.map((link, linkIdx) => (
                 <li
@@ -131,14 +132,14 @@ const FourOhfour = () => {
                   <div className="flex-shrink-0">
                     <span className="flex items-center justify-center h-12 w-12 rounded-lg bg-indigo-50">
                       <link.icon
-                        className="h-6 w-6 text-indigo-700"
+                        className="h-6 w-6 text-mango-yellow"
                         aria-hidden="true"
                       />
                     </span>
                   </div>
                   <div className="min-w-0 flex-1">
-                    <h3 className="text-base font-medium text-gray-900">
-                      <span className="rounded-sm focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                    <h3 className="text-base font-medium text-white">
+                      <span className="rounded-sm focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-mango-yellow">
                         <a href="#" className="focus:outline-none">
                           <span
                             className="absolute inset-0"
@@ -148,13 +149,13 @@ const FourOhfour = () => {
                         </a>
                       </span>
                     </h3>
-                    <p className="text-base text-gray-500">
+                    <p className="text-base text-white text-opacity-50">
                       {link.description}
                     </p>
                   </div>
                   <div className="flex-shrink-0 self-center">
                     <ChevronRightIcon
-                      className="h-5 w-5 text-gray-400"
+                      className="h-5 w-5 text-mango-yellow"
                       aria-hidden="true"
                     />
                   </div>
@@ -164,7 +165,7 @@ const FourOhfour = () => {
             <div className="mt-8">
               <a
                 href="#"
-                className="text-base font-medium text-indigo-600 hover:text-indigo-500"
+                className="text-base font-medium text-mango-orange hover:text-mango-yellow"
               >
                 Or go back home<span aria-hidden="true"> &rarr;</span>
               </a>
