e48a4fa0dc06e0c6d0c75f5406ee58521431d23a
steven <sarmiento.steven@gmail.com>
2022-12-14 12:34:33 -0500

    The start of something new new

--- pages/index_teaser.tsx
+++ pages/index_teaser.tsx
@@ -1,16 +1,12 @@
-import NavBarAlpha from '../components/NavBarAlpha'
-import HeroSectionNew from '../components/HeroSectionNew'
-import HeroSectionMarkets from '../components/HeroSectionMarkets'
-//import HeroSectionFeatures from '../components/HeroSectionFeatures'
+import NavBarBeta from '../components/NavBarBeta'
+import HeroSectionHome from '../components/HeroSectionHome'
 
 const IndexTeaser = () => {
   return (
     <div className={`bg-bkg-1 text-fgd-1 transition-all overflow-hidden`}>
       <div className="w-screen h-2 bg-gradient-to-r from-mango-red via-mango-yellow to-mango-green"></div>
-      <NavBarAlpha />
-      <HeroSectionNew />
-      <HeroSectionMarkets />
-      {/*<HeroSectionFeatures />*/}
+      <NavBarBeta />
+      <HeroSectionHome />
     </div>
   )
 }
