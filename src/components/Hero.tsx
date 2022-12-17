a1b03f33076791bb3353e3ee043109f42d9e0a85
steven <sarmiento.steven@gmail.com>
2022-12-17 15:12:52 -0500

    trying things

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,20 +4,22 @@ import GradientText from '@/components/GradientText';
 const Hero = () => {
   return (
     <>
-    <div>
-      <p className="font-display xs:text-3xl mb-3 text-center font-bold text-white sm:text-4xl md:text-4xl lg:text-5xl">
-        Every market, all the power.
-        <br />
-        <GradientText>None of the fuss.</GradientText>
-      </p>
-    </div>
-    <div>
-       <img
-         className="z-0 hidden md:block absolute left-0 top-96 w-full h-auto"
-         src={`/assets/images/hero_desktop.png`}          
-         alt=""
-        />
-    </div>
+    <section className="bg-mango-bkg-1">
+      <div>
+        <p className="font-display xs:text-3xl mb-3 text-center font-bold text-white sm:text-4xl md:text-4xl lg:text-5xl">
+          Every market, all the power.
+          <br />
+          <GradientText>None of the fuss.</GradientText>
+        </p>
+      </div>
+      <div>
+        <img
+          className="z-0 hidden md:block absolute left-0 top-96 w-full h-auto"
+          src={`/assets/images/hero_desktop.png`}          
+          alt=""
+          />
+      </div>
+    </section>
     </>
   );
 };
