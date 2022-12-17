35eed77694d8af48ac2b5e9b3404ca1cd4825ecf
steven <sarmiento.steven@gmail.com>
2022-12-17 17:36:42 -0500

    little bits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,41 @@
 
 import GradientText from '@/components/GradientText';
+import Button from '@/components/Button';
+import Linkie from '@/components/Linkie';
 
 const Hero = () => {
   return (
     <>
-    <section className="bg-mango-bkg-1">
-      <div>
+      <div className="mt-20 mb-8 z-10">
         <p className="font-display xs:text-3xl mb-3 text-center font-bold text-white sm:text-4xl md:text-4xl lg:text-5xl">
           Every market, all the power.
           <br />
           <GradientText>None of the fuss.</GradientText>
         </p>
       </div>
+        <div className="mt-8 flex lg:flex-row md:flex-row lg:justify-center md:justify-center sm:justify-center">
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://trade.mango.markets"
+                      >
+                        <Button>Check out 🥭</Button>
+                      </a>
+                      <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://docs.mango.markets/"
+                      >
+                        <Linkie>Explore the docs</Linkie>
+                      </a>
+        </div>
       <div>
         <img
-          className="z-0 hidden md:block absolute left-0 top-96 w-full h-auto"
+          className="z-0 hidden md:block absolute left-0 top-96 mt-40 w-full h-auto"
           src={`/assets/images/hero_desktop.png`}          
           alt=""
           />
       </div>
-    </section>
     </>
   );
 };
---
+++ src/components/Linkie.tsx
@@ -0,0 +1,53 @@
+import { FunctionComponent } from 'react';
+import React from 'react';
+import styled from '@emotion/styled';
+import tw from 'twin.macro';
+
+const StyledButton = styled.a`
+  font-weight: 700;
+  cursor: pointer;
+
+  :hover {
+    ${tw`no-underline`}
+  }
+
+  :disabled {
+    ${tw`cursor-not-allowed opacity-60`}
+  }
+`
+interface LinkieProps {
+  className?: string
+  children: React.ReactNode
+}
+
+const Linkie: FunctionComponent<LinkieProps> = ({
+  children,
+  className,
+  ...props
+
+}) => {
+  return (
+    
+    <>
+      <div className='text-mango-fgd-3 hover:text-white duration-300'>
+        <StyledButton
+          className={`${className} pr-5 flex z-10 px-8 py-2 text-fgd-1 relative rounded-full`}
+          {...props}
+        >
+          {children}
+          <div>
+            <svg 
+                  xmlns="http://www.w3.org/2000/svg" 
+                  viewBox="0 0 20 20" 
+                  fill="currentColor" 
+                  className="w-6 h-6">
+              <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
+            </svg>
+          </div>
+        </StyledButton>
+      </div>
+    </>
+  )
+}
+
+export default Linkie;
