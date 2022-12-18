79371c1428c56df5343f7b44f5e36d995dabd87a
steven <sarmiento.steven@gmail.com>
2022-12-17 21:57:24 -0500

    moar tab stuffs

--- src/components/FeaturesOneTabs.tsx
+++ src/components/FeaturesOneTabs.tsx
@@ -3,24 +3,48 @@ import { Tab } from '@headlessui/react'
 function FeaturesOneTabs() {
   return (
     <Tab.Group vertical>
-      <Tab.List className={"text-white bg-slate-500 "}>
-        <Tab className={"hover:bg-slate-700 "}>
+      <Tab.List className={"text-white bg-slate-500 flex flex-col"}>
+        <Tab className={"hover:bg-slate-700 focus:bg-mango-yellow "}>
           <p>Maximum capital efficiency</p>
           <p>Utilize all your assets as collateral to trade any other asset with up to 5x leverage. </p>
         </Tab>
-        <Tab className={"hover:bg-slate-700 "}>
+        <Tab className={"hover:bg-slate-700 focus:bg-mango-yellow"}>
           <p>Maximum capital efficiency</p>
           <p>Utilize all your assets as collateral to trade any other asset with up to 5x leverage. </p>
         </Tab>
-        <Tab className={"hover:bg-slate-700 "}>
+        <Tab className={"hover:bg-slate-700 focus:bg-mango-yellow"}>
           <p>Maximum capital efficiency</p>
           <p>Utilize all your assets as collateral to trade any other asset with up to 5x leverage. </p>
         </Tab>
       </Tab.List>
-      <Tab.Panels className={"text-white bg-slate-700 "}>
-        <Tab.Panel>Content 1</Tab.Panel>
-        <Tab.Panel>Content 2</Tab.Panel>
-        <Tab.Panel>Content 3</Tab.Panel>
+      <Tab.Panels className={"text-white"}>
+        <Tab.Panel>
+        <div>
+          <img
+            className="z-0 reletive w-full h-auto"
+            src={`/assets/images/ipad_one.png`}          
+            alt=""
+            />
+        </div>
+        </Tab.Panel>
+        <Tab.Panel>
+        <div>
+          <img
+            className="z-0 reletive w-full h-auto"
+            src={`/assets/images/ipad_two.png`}          
+            alt=""
+            />
+        </div>
+        </Tab.Panel>
+        <Tab.Panel>
+        <div>
+          <img
+            className="z-0 reletive w-full h-auto"
+            src={`/assets/images/ipad_three.png`}          
+            alt=""
+            />
+        </div>
+        </Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
   )
