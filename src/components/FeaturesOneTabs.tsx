be72c1ceb362b306a5a926c3694ec0c8262d83db
steven <sarmiento.steven@gmail.com>
2022-12-19 19:00:33 -0500

    tabby tabby not too shabby

--- src/components/FeaturesOneTabs.tsx
+++ src/components/FeaturesOneTabs.tsx
@@ -1,22 +1,124 @@
 import { Tab } from '@headlessui/react'
 
+//import {
+//  ReceiptTaxIcon,
+//  LightningBoltIcon,
+//  CurrencyDollarIcon,
+// } from '@heroicons/react/outline'
+
+const featureOne = [
+  {
+    name: 'Maximum capital efficiency.',
+    description:
+      "Every market is cross-collateralized. Mango's risk engine let's you utilize all your assets and derivative contracts as collateral.",
+    //icon: CurrencyDollarIcon,
+  },
+]
+const featureTwo = [
+  {
+    name: 'Sub-second latency.',
+    description:
+      'Instant order execution at the price of less than a cent. Low latency allows market makers to post tight spreads on the books. Oh, and liquidations are instant as well, so watch your exposure.',
+   //icon: LightningBoltIcon,
+  },
+]
+const featureThree = [
+  {
+    name: 'The lowest fees.',
+    description:
+      'Trade with the lowest fee possible. SRM deposits are pooled for a collective discount. Mango is the first protocol to charge zero fees on margin borrowing & lending.',
+   // icon: ReceiptTaxIcon,
+  },
+]
+
 function FeaturesOneTabs() {
   return (
+    <>
     <Tab.Group vertical>
-      <Tab.List className={"text-white bg-white bg-opacity-5 flex flex-col w-auto"}>
-        <Tab className={"hover:bg-slate-700 focus:bg-mango-yellow "}>
-          <p>Maximum capital efficiency</p>
-          <p>Utilize all your assets as collateral to trade any other asset with up to 5x leverage. </p>
-        </Tab>
-        <Tab className={"hover:bg-slate-700 focus:bg-mango-yellow"}>
-          <p>Maximum capital efficiency</p>
-          <p>Utilize all your assets as collateral to trade any other asset with up to 5x leverage. </p>
-        </Tab>
-        <Tab className={"hover:bg-slate-700 focus:bg-mango-yellow"}>
-          <p>Maximum capital efficiency</p>
-          <p>Utilize all your assets as collateral to trade any other asset with up to 5x leverage. </p>
+      <Tab.List className={"text-white flex flex-col w-450"}>
+          <Tab className={"bg-white bg-opacity-0 hover:bg-opacity-5 focus:bg-opacity-5 p-8 rounded-lg mt-4 mb-4"}>
+        
+          <dl className="group">
+            {featureOne.map((featureOne) => (
+              <div className="text-left" key={featureOne.name}>
+                <dt>
+                  {/* 
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-bkg-3 text-mango-yellow">
+                    <feature.icon
+                      className="h-6 w-6"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  */}
+                  <p className="text-xl font-semibold font-heading">
+                    {featureOne.name}
+                  <span className="text-mango-yellow opacity-0 transition duration-150 ease-in group-hover:opacity-100 ml-2" aria-hidden="true">&rarr;</span>
+                  </p>
+                </dt>
+                <dd className="mt-2 text-base text-white opacity-50">
+                  {featureOne.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+
+          </Tab>
+          <Tab className={"bg-white bg-opacity-0 hover:bg-opacity-5 focus:bg-opacity-5 p-8 rounded-lg mt-4 mb-4"}>
+        
+          <dl className="group">
+            {featureTwo.map((featureTwo) => (
+              <div className="text-left" key={featureTwo.name}>
+                <dt>
+                  {/* 
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-bkg-3 text-mango-yellow">
+                    <feature.icon
+                      className="h-6 w-6"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  */}
+                  <p className="text-xl font-semibold font-heading">
+                    {featureTwo.name}
+                  <span className="text-mango-yellow opacity-0 transition duration-150 ease-in group-hover:opacity-100 ml-2" aria-hidden="true">&rarr;</span>
+                  </p>
+                </dt>
+                <dd className="mt-2 text-base text-white opacity-50">
+                  {featureTwo.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+
+          </Tab>
+          <Tab className={"bg-white bg-opacity-0 hover:bg-opacity-5 focus:bg-opacity-5 p-8 rounded-lg mt-4 mb-4"}>
+
+          <dl className="group">
+            {featureThree.map((featureThree) => (
+              <div className="text-left" key={featureThree.name}>
+                <dt>
+                  {/* 
+                  <div className="flex items-center justify-center h-12 w-12 rounded-md bg-bkg-3 text-mango-yellow">
+                    <feature.icon
+                      className="h-6 w-6"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  */}
+                  <p className="text-xl font-semibold font-heading">
+                    {featureThree.name}
+                  <span className="text-mango-yellow opacity-0 transition duration-150 ease-in group-hover:opacity-100 ml-2" aria-hidden="true">&rarr;</span>
+                  </p>
+                </dt>
+                <dd className="mt-2 text-base text-white opacity-50">
+                  {featureThree.description}
+                </dd>
+              </div>
+            ))}
+          </dl>
+
         </Tab>
       </Tab.List>
+
       <Tab.Panels className={"text-white"}>
         <Tab.Panel>
         <div>
@@ -47,7 +149,7 @@ function FeaturesOneTabs() {
         </Tab.Panel>
       </Tab.Panels>
     </Tab.Group>
-    
+    </>
   )
 }
 
--- src/components/StyleGuideMenu.tsx
+++ src/components/StyleGuideMenu.tsx
@@ -2,34 +2,56 @@ const Logo = () => {
     return (
         <>
         <div className="w-full grid grid-cols-1 overflow-hidden rounded-lg border border-white border-opacity-10 shadow-lg">
-        <div className="relative grid gap-4 bg-mango-bkg-3 px-5 py-4 sm:gap-4 sm:p-6 backdrop-blur-3xl bg-opacity-50">
-          <h3 className="text-sm font-bold uppercase tracking-wide text-white opacity-40"> 
-            Style Guide
-          </h3>
+        <div className="relative grid gap-4 bg-mango-bkg-3 px-5 py-4 sm:gap-4 sm:p-6 backdrop-blur-3xl bg-opacity-50 ">
           <button className="text-left hover:text-mango-yellow text-mango-fgd-5 group">
             <a
               href="https://trade.mango.markets/"
               className="-m-3 flex items-start rounded-lg p-3 transition duration-150 ease-in-out hover:bg-white hover:backdrop-blur-xl hover:bg-opacity-5"
             >
-              <svg
+            <svg 
                 className="h-5 w-5 shrink-0 stroke-current mt-1"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 22 22"
                 stroke="currentColor"
                 aria-hidden="true"
-              >
-                <path
-                  d="M6 12V15M10 12V15M14 12V15M1 19H19M1 8H19M1 5L10 1L19 5M2 8H18V19H2V8Z"
+                >
+            <path 
                   strokeWidth="2"
                   strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-
+                  strokeLinejoin="round" 
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+            </svg>
+            
               <div className="ml-4">
                 <p className="text-base font-medium text-white">
-                  Markets<span className="text-white opacity-0 transition duration-150 ease-in group-hover:opacity-50 ml-2" aria-hidden="true">&rarr;</span>
+                  Logo Light<span className="text-white opacity-0 transition duration-150 ease-in group-hover:opacity-50 ml-2" aria-hidden="true">&rarr;</span>
+                </p>
+              </div>
+            </a>
+          </button>
+          <button className="text-left hover:text-mango-yellow text-mango-fgd-5 group">
+            <a
+              href="https://trade.mango.markets/"
+              className="-m-3 flex items-start rounded-lg p-3 transition duration-150 ease-in-out hover:bg-white hover:backdrop-blur-xl hover:bg-opacity-5"
+            >
+            <svg 
+                className="h-5 w-5 shrink-0 stroke-current mt-1"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 22 22"
+                stroke="currentColor"
+                aria-hidden="true"
+                >
+            <path 
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round" 
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+            </svg>
+              <div className="ml-4">
+                <p className="text-base font-medium text-white">
+                  Logo Dark<span className="text-white opacity-0 transition duration-150 ease-in group-hover:opacity-50 ml-2" aria-hidden="true">&rarr;</span>
                 </p>
               </div>
             </a>
