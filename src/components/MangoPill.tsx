d6ce8bc60f66dea66d0b6540557742ceb834fd8e
steven <sarmiento.steven@gmail.com>
2022-12-20 19:15:06 -0500

    pills are good

--- src/components/MangoPill.tsx
+++ src/components/MangoPill.tsx
@@ -1,7 +1,7 @@
 const MangoPill = (props: any) => (
   <div
-    className="inline-flex h-6 items-center rounded-full bg-gradient-to-br from-mango-red
- to-mango-orange p-2"
+    className="relative z-20 mb-4 inline-flex h-6 items-center rounded-full bg-gradient-to-br from-mango-red
+ to-mango-orange p-2 transform translate-y-4"
   >
     <p className="text-xs font-bold uppercase tracking-widest text-white subpixel-antialiased">
       {props.children}
