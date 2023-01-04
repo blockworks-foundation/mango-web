37fbea8559228550fbb25118acc95225572faeda
steven <sarmiento.steven@gmail.com>
2023-01-04 04:09:49 -0800

    ze blob

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,11 +1,14 @@
 import GradientText from "@/components/GradientText";
+import MangoBlob from "@/components/MangoBlob"
 
 const CallToAction = () => {
   return (
     <>
-
+<div className="z-0 transform -translate-y-40 translate-x-32">
+           <MangoBlob />
+          </div> 
 <div className="z-50 mx-auto max-w-7xl pt-0 pb-16 my-16 mt-0">
-    <div className="group h-72 transform transition duration-200 ease-in-out hover:-translate-y-2 bg-th-bkg-4 border border-th-fgd-2 rounded-[40px] shadow-md overflow-hidden lg:grid lg:grid-cols-2 lg:gap-2 mt-8 bg-mangotop bg-cover bg-top bg-no-repeat">
+    <div className="group h-72 transform transition duration-200 ease-in-out hover:-translate-y-2 bg-th-bkg-4 border border-th-fgd-2 rounded-[40px] shadow-md overflow-hidden lg:grid lg:grid-cols-2 lg:gap-2 mt-8 bg-mangotop bg-cover bg-top bg-no-repeat backdrop-blur-xl bg-opacity-50">
     <div className="pt-10 pb-12 px-5 sm:pt-16 sm:px-16 lg:py-4 lg:pr-0 xl:py-12 xl:px-16">
         <div className="lg:self-center">
         <h2 className="text-4xl font-mono text-white w-screen">
