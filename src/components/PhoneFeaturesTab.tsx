395351e27c282c13080ec85185407217aeb8f72b
steven <sarmiento.steven@gmail.com>
2023-01-06 14:17:48 -0800

    adding some arrows to the phone tabs

--- src/components/PhoneFeaturesTab.tsx
+++ src/components/PhoneFeaturesTab.tsx
@@ -62,7 +62,13 @@ function PhoneFeaturesTabs() {
                                 <p className="text-xl font-semibold font-heading">
                                 {featureOne.name}
                                 </p>
-
+                                <svg 
+                              xmlns="http://www.w3.org/2000/svg" 
+                              viewBox="0 0 20 20" 
+                              fill="currentColor" 
+                              className="w-8 h-8 group-hover:opacity-100 text-mango-yellow opacity-0 transform -translate-x-6 group-hover:translate-x-0 transition duration-150 ease-in">
+                          <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
+                        </svg>
                             </div>
 
                         </dt>
@@ -96,7 +102,13 @@ function PhoneFeaturesTabs() {
                             <p className="text-xl font-semibold font-heading">
                                 {featureTwo.name}
                             </p>
-
+                            <svg 
+                              xmlns="http://www.w3.org/2000/svg" 
+                              viewBox="0 0 20 20" 
+                              fill="currentColor" 
+                              className="w-8 h-8 group-hover:opacity-100 text-mango-yellow opacity-0 transform -translate-x-6 group-hover:translate-x-0 transition duration-150 ease-in">
+                          <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
+                        </svg>
                             </div>
 
                         </dt>
@@ -129,7 +141,13 @@ function PhoneFeaturesTabs() {
                             <p className="text-xl font-semibold font-heading">
                                 {featureThree.name}
                             </p>
-
+                            <svg 
+                              xmlns="http://www.w3.org/2000/svg" 
+                              viewBox="0 0 20 20" 
+                              fill="currentColor" 
+                              className="w-8 h-8 group-hover:opacity-100 text-mango-yellow opacity-0 transform -translate-x-6 group-hover:translate-x-0 transition duration-150 ease-in">
+                          <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
+                        </svg>
                             </div>
                         </dt>
                         <dd className="mt-2 text-base text-th-fgd-3 transition duration-75 ease-in group-hover:text-th-fgd-5">
