61faec0b4661c96c6d0067df025dd5e78ca56ec9
steven <sarmiento.steven@gmail.com>
2023-01-13 01:03:24 -0800

    little hero change

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -4,17 +4,18 @@ import Button from '@/components/Button';
 import Linkie from '@/components/Linkie';
 import MangoBlob from '@/components/MangoBlob';
 import BrowserMockupAbstract from '@/components/mockups/BrowserMockupAbstract';
+import IphoneAbstract from '@/components/mockups/IphoneAbstract';
 
 const HomeHero = () => {
   return (
-    <div className="mt-72 mb-8 z-10">
-        <p className="font-display font-bold text-2xl mb-3 text-center text-th-fgd-5 sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl">
+      <div className="mt-60 mb-8 z-10 max-w-7xl mx-auto p-8 py-20">
+        <p className="z-20 font-display text-2xl mb-3 text-left font-bold text-th-fgd-5 sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl">
           Every market, all the power.
           <br />
           <GradientText>None of the fuss.</GradientText>
         </p>
 
-        <div className="mt-8 flex flex-col lg:flex-row md:flex-row lg:justify-center md:justify-center sm:justify-center">
+        <div className="mt-8 inline-flex z-20">
           <Button>
             <a rel="noreferrer" target="_blank" href="https://trade.mango.markets">
               Discover 🥭
@@ -28,11 +29,21 @@ const HomeHero = () => {
         </div>
 
       <div className="flex flex-col">
-        <div className="z-0 transform -translate-y-64 translate-x-56">
+        <div className="z-0 transform -translate-y-[150px] -translate-x-[125px]">
           <MangoBlob />
         </div>
-        <div className="z-10 hidden md:block reletive left-0 top-96 mt-0 ">
+        <div className="z-10 block reletive transform -translate-y-[300px] translate-x-[550px] -rotate-12 ">
           <BrowserMockupAbstract />
+{/* 
+          <img
+            className="w-full h-auto"
+            src="/assets/images/hero_desktop.png"
+            alt="desktop"
+          />
+*/}          
+        </div>
+        <div className="z-10 block reletive transform -translate-y-[766px] -translate-x-[77px] -rotate-12 ">
+            <IphoneAbstract />
 {/* 
           <img
             className="w-full h-auto"
---
+++ src/components/mockups/IphoneAbstract.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function IphoneAbstract(props: any) {
+  return (
+    <div>
+                    {/* PHone in CSS */}
+                    <div className="-mt-24 m-auto rounded-[50px] w-[310px] shadow-[0_8px_42px_-5px_rgba(0,0,0,0.35)]">
+                            <span className="h-[30px] w-1 absolute bg-th-fgd-2 mt-[120px] rounded-l-full -ml-1"></span>
+                            <span className="transform translate-y-[150px] translate-x-[310px] h-[80px] w-1 absolute bg-th-fgd-2 rounded-r-full"></span>
+                            <span className="h-[60px] w-1 absolute bg-th-fgd-3 mt-[160px] rounded-l-full -ml-1"></span>
+                            <span className="h-[60px] w-1 absolute bg-th-fgd-3 mt-[230px] rounded-l-full -ml-1"></span>
+
+                            <div className="bg-th-fgd-1 bg-opacity-0 h-[620px] w-[310px] rounded-[50px] backdrop-blur-xl border-[5px] border-solid border-th-bkg-4 border-opacity-10">           
+                                <div className="h-[611px] border-[5px] border-solid border-th-bkg-3 rounded-[46px]">
+                                    <div className="flex items-center flex-col">
+                                        <ul className="absolute">
+                                            <li className="bg-black bg-opacity-80 h-[20px] w-[90px] rounded-full mt-6"></li>
+                                        </ul>
+                                    </div>
+                                     <img 
+                                        className="z-0 reletive w-full h-full rounded-[41px]"
+                                        src={props.src} />
+                                    <div className="flex items-center flex-col">
+                                        <ul className="absolute">
+                                            <li className="bg-white bg-opacity-80 h-[6px] w-[125px] rounded-full -mt-6 animate-bounce"></li>
+                                        </ul>
+                                    </div>   
+                                </div>
+                                </div> 
+                        </div>
+
+    </div>
+  );
+}
+
+export default IphoneAbstract;
