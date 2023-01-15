4b1a5b2706293ceac7fc4b4635c2f6bfa8876232
steven <sarmiento.steven@gmail.com>
2023-01-15 15:04:28 -0800

    words

--- src/components/SectionCta.tsx
+++ src/components/SectionCta.tsx
@@ -9,9 +9,9 @@ const SectionCta = () => {
      <div className="my-72">
       <div className="z-10">
       <h2 className="text-center font-display text-6xl tracking-tight mb-6 text-th-fgd-5 leading-tight">
-          Access to what you need.
+          Powered by a fruit.
           <br />
-          <GradientText>The quick brown fox jumped.</GradientText>
+          <GradientText>For everyone to enjoy.</GradientText>
         </h2>
       </div>
         <div className="mt-8 flex lg:flex-row md:flex-row lg:justify-center md:justify-center sm:justify-center">
