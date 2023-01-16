37e1be6672f4747eb3d1b9b00718b5351b9fc7e8
steven <sarmiento.steven@gmail.com>
2023-01-16 15:48:35 -0800

    smaller boootons

--- src/components/mockups/BrowserMockupAbstract.tsx
+++ src/components/mockups/BrowserMockupAbstract.tsx
@@ -14,15 +14,15 @@ function BrowserMockupAbstract() {
                 </ul>
             </div>
 
-            <div className="flex flex-row bg-th-bkg-3 w-full py-4 pl-4 backdrop-blur-xl border border-1 border-solid border-th-fgd-1">
+            <div className="flex flex-row bg-th-bkg-3 w-full py-2 pl-4 backdrop-blur-xl border border-1 border-solid border-th-fgd-1">
               <div>
-                <ul className="flex flex-row space-x-3">
+                <ul className="flex flex-row space-x-2 mt-[3px]">
                   <li className="bg-th-fgd-2 p-1 rounded-full text-th-fgd-3">
                     <svg 
                       xmlns="http://www.w3.org/2000/svg" 
                       viewBox="0 0 24 24" 
                       fill="currentColor" 
-                      className="w-6 h-6">
+                      className="w-5 h-5">
                       <path 
                       fillRule="evenodd" 
                       d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" 
@@ -35,7 +35,7 @@ function BrowserMockupAbstract() {
                       xmlns="http://www.w3.org/2000/svg" 
                       viewBox="0 0 24 24" 
                       fill="currentColor" 
-                      className="w-6 h-6">
+                      className="w-5 h-5">
                       <path 
                         fillRule="evenodd" 
                         d="M12.97 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06l6.22-6.22H3a.75.75 0 010-1.5h16.19l-6.22-6.22a.75.75 0 010-1.06z" 
@@ -48,7 +48,7 @@ function BrowserMockupAbstract() {
                       xmlns="http://www.w3.org/2000/svg" 
                       viewBox="0 0 24 24" 
                       fill="currentColor" 
-                      className="w-6 h-6">                  
+                      className="w-5 h-5">                  
                     <path 
                         fillRule="evenodd" 
                         d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z" 
