0fa4111e96a4af9fd4568d43f1cde6fadae30060
steven <sarmiento.steven@gmail.com>
2023-01-19 19:25:34 -0800

    words

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -33,7 +33,7 @@ const HomeHero = () => {
         <ul className="flex flex-col z-20 font-body text-xl mb-3 text-left">
          <li className="group mb-2 inline-flex text-th-fgd-3 hover:text-th-fgd-4 transition-all duration-100 ease-in-out cursor-default">
             <FireIcon className="bg-th-fgd-1 group-hover:bg-th-fgd-2 rounded-md p-1 w-[30px] h-[30px] group-hover:text-mango-yellow mr-3 mb-4" />
-            Blazing fast products, built transparently and all on-chain.
+            Blazing fast products, transparent and all on-chain.
           </li>
 
           <li className="group mb-2 inline-flex text-th-fgd-3 hover:text-th-fgd-4 transition-all duration-100 ease-in-out cursor-default">
