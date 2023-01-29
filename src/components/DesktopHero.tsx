505f815cc16a299a0f52fc28eb96aef0519617fa
steven <sarmiento.steven@gmail.com>
2023-01-29 01:51:55 -0800

    bits

--- src/components/DesktopHero.tsx
+++ src/components/DesktopHero.tsx
@@ -19,7 +19,7 @@ const DesktopHero = () => {
         <div className="w-1/2 h-[4px] bg-gradient-to-r from-th-fgd-2 transform -translate-x-[32px] mb-3"></div>
 
         <h2 className="font-display sm:text-6xl text-2xl tracking-tight mb-6 text-left text-th-fgd-5 leading-tight">
-        Be merry, we&apos;re back.<br /> <GradientText>Trade, manage, earn <br />all from one hub.</GradientText>
+        Every market, all the power.<br /> <GradientText>Trade, manage, earn <br /> from one hub.</GradientText>
         </h2>
         <div className="w-1/2 h-[4px] bg-gradient-to-r from-th-fgd-2 transform -translate-x-[32px] mb-8"></div>
 
