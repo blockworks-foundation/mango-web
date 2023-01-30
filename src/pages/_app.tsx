b263e32565f2dc8d546e66e28f9096670b21a934
steven <sarmiento.steven@gmail.com>
2023-01-29 20:22:09 -0800

    removed animation, too laggy rn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/global.css'
 import { ThemeProvider } from 'next-themes'
-import { AnimatePresence, motion } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import { THEME_KEY } from '../utils/constants'
 import NavvyAlpha from '@/components/NavvyAlpha'
@@ -8,29 +7,7 @@ import NavvyAlpha from '@/components/NavvyAlpha'
 const MangoWeb = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider defaultTheme="Mango" storageKey={THEME_KEY}>
     <NavvyAlpha />
-    <AnimatePresence mode="wait">
-      <motion.div        
-        initial="initialState"
-        animate="animateState"
-        exit="exitState"
-        transition={{
-          duration:0.50,
-        }}
-        variants={{
-          initialState: {
-            opacity: .5,
-            y:50,
-          },
-          animateState: {
-            opacity: 1,
-            y:0,
-          },
-        }}
-        className=""
-        >
             <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
   </ThemeProvider>
 
 
