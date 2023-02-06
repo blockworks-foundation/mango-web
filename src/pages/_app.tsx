9227cdbb76232267cfddfd612d9b30e0803088ca
steven <sarmiento.steven@gmail.com>
2023-02-06 14:28:59 -0800

    adding analytics code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import { THEME_KEY } from '../utils/constants'
 import NavvyAlpha from '@/components/NavvyAlpha'
+import { Analytics } from '@vercel/analytics/react';
 
 const MangoWeb = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider defaultTheme="Mango" storageKey={THEME_KEY}>
     <NavvyAlpha />
             <Component {...pageProps} />
+            <Analytics />
   </ThemeProvider>
 
 
