4eb6d825380241cf8fc8fb4cb5baac93195e7742
steven <sarmiento.steven@gmail.com>
2023-02-13 19:48:49 -0800

    rounded-full bb, ode to the  first button

--- src/components/ButtonTwo.tsx
+++ src/components/ButtonTwo.tsx
@@ -19,15 +19,15 @@ const ButtonTwo: FunctionComponent<ButtonProps> = ({
     <div className="group">
 
       <div className="flex justify-center">
-        <button className=" transition-all duration-100 ease-in-out bg-th-fgd-2 rounded-xl">
+        <button className=" transition-all duration-100 ease-in-out bg-th-fgd-2 rounded-full">
             <span
-              className="z-30 relative inline-flex items-center justify-center p-4 px-[3px] py-[3px] overflow-hidden font-bold font-display rounded-xl shadow-2xl group">
-              <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-300 group-hover:bg-mango-yellow rounded-xl blur-lg ease-in-out"></span>
+              className="z-30 relative inline-flex items-center justify-center p-4 px-[3px] py-[3px] overflow-hidden font-bold font-display rounded-full shadow-2xl group">
+              <span className="absolute top-0 left-0 w-40 h-40 -mt-10 -ml-3 transition-all duration-300 group-hover:bg-mango-yellow rounded-full blur-lg ease-in-out"></span>
               <span className="absolute inset-0 w-full h-full transition duration-300 group-hover:rotate-180 ease-in-out">
               <span className="absolute bottom-0 left-0 w-24 h-24 -ml-10 group-hover:bg-mango-green rounded-full blur-xl"></span>
               <span className="absolute bottom-0 right-0 w-24 h-24 -mr-10 group-hover:bg-mango-red rounded-full blur-xl"></span>
               </span>
-              <span className=" relative text-th-fgd-3 bg-th-bkg-3 py-2 px-7 rounded-[9px] hover:bg-th-bkg-1 hover:text-th-fgd-5 transition-all duration-150 ease-in">
+              <span className=" relative text-th-fgd-3 bg-th-bkg-3 py-2 px-7 rounded-full hover:bg-th-bkg-1 hover:text-th-fgd-5 transition-all duration-150 ease-in">
 
           {children}
           </span>
