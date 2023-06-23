886f10526c503f3cd1aee9ec3788c08c2e0ca4e1
saml33 <slam.uke@gmail.com>
2023-06-23 10:13:42 +1000

    update meta title/description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,17 @@ import '../styles/index.css'
 import LayoutWrapper from '../components/layout/LayoutWrapper'
 import { appWithTranslation } from 'next-i18next'
 
-function App({ Component, pageProps }) {
-  const title = 'Mango Markets'
-  const description =
-    'A magical new way to interact with DeFi. Groundbreaking safety features designed to keep your funds secure.'
-  const keywords =
-    'Mango Markets, DEFI, Decentralized Finance, Decentralized Finance, Crypto, ERC20, Ethereum, Solana, SOL, SPL, Cross-Chain, Trading, Fastest, Fast, SPL Tokens'
+const metaTitle = 'Mango Markets – Safer. Smarter. Faster.'
+const metaDescription =
+  'A magical new way to interact with DeFi. Groundbreaking safety features designed to keep your funds secure. The easiest way to margin trade any token pair. All powered by flashloans.'
+const keywords =
+  'Mango Markets, DEFI, Decentralized Finance, Decentralized Finance, Crypto, ERC20, Ethereum, Solana, SOL, SPL, Cross-Chain, Trading, Fastest, Fast, SPL Tokens'
 
+function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>Mango Markets</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
@@ -23,13 +23,14 @@ function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content={keywords} />
-        <meta name="description" content={description} />
+        <meta property="og:title" content={metaTitle} />
+        <meta name="description" content={metaDescription} />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Mango Markets" />
-        <meta name="twitter:description" content={description} />
+        <meta name="twitter:title" content={metaTitle} />
+        <meta name="twitter:description" content={metaDescription} />
         <meta
           name="twitter:image"
           content="https://mango.markets/twitter-card.png?123456789"
