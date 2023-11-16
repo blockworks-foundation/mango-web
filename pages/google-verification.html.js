a6ff868143b350f028944b678c581fb65de839e9
saml33 <slam.uke@gmail.com>
2023-11-16 11:17:28 +1100

    google site verification

--- pages/google-verification.html.js
+++
@@ -1,9 +0,0 @@
-export default class extends React.Component {
-  static async getInitialProps({ res }) {
-    res.setHeader('Content-Type', 'text/html')
-    res.write(
-      '<html><body>google-site-verification: google1c604c9025f210a7.html</body></html>'
-    )
-    res.end()
-  }
-}
