ef1776db2ee1aae09a44085d8bee27e6f8329a85
saml33 <slam.uke@gmail.com>
2023-12-12 21:50:12 +1100

    add more metadata

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -2,10 +2,29 @@ import { Metadata } from 'next'
 import HomePage from '../components/home/HomePage'
 import { fetchAppStatsData, fetchMangoMarketData } from '../utils/mango'
 
+const metaTitle = 'Mango Markets | Safer. Smarter. Faster.'
+const metaDescription =
+  'A magical new way to interact with DeFi. Groundbreaking safety features designed to keep your funds secure. The easiest way to margin trade any token pair. All powered by flashloans.'
+
 export const metadata: Metadata = {
-  title: 'Mango Markets',
-  description:
-    'A magical new way to interact with DeFi. Groundbreaking safety features designed to keep your funds secure. The easiest way to margin trade any token pair. All powered by flashloans.',
+  title: metaTitle,
+  description: metaDescription,
+  openGraph: {
+    title: metaTitle,
+    description: metaDescription,
+    url: 'https://mango.markets',
+    siteName: 'Mango Markets',
+    images: [
+      {
+        url: 'https://mango.markets/twitter-card.png',
+        width: 1200,
+        height: 600,
+        alt: metaTitle,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 async function Page() {
