b0eada3c8ce3d5f11ae8270df35d775c01337fac
saml33 <slam.uke@gmail.com>
2024-01-02 13:30:37 +1100

    add preview mode banner

--- app/components/ExitDraftModeLink.tsx
+++ app/components/ExitDraftModeLink.tsx
@@ -6,7 +6,11 @@ function ExitDraftModeLink(props: React.HTMLProps<HTMLAnchorElement>) {
   const pathname = usePathname()
 
   return (
-    <a href={`/api/disable-draft?redirect=${pathname}`} {...props}>
+    <a
+      className="text-black underline text-sm font-bold tracking-normal md:hover:no-underline md:hover:text-black"
+      href={`/api/disable-draft?redirect=${pathname}`}
+      {...props}
+    >
       Exit
     </a>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './styles/global.css'
 import LayoutWrapper from './components/LayoutWrapper'
+import ExitDraftModeLink from './components/ExitDraftModeLink'
+import { draftMode } from 'next/headers'
 
 export default function RootLayout({
   children,
@@ -9,6 +11,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        {draftMode().isEnabled && (
+          <div className="bg-orange-200 py-2 px-6 flex justify-center">
+            <span>
+              Preview mode enabled. <ExitDraftModeLink />
+            </span>
+          </div>
+        )}
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
