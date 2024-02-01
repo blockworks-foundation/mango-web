64b1c68952d9973b0882885a31e3d1de87f46aea
saml33 <slam.uke@gmail.com>
2024-02-01 12:08:23 +1100

    add gofx logo

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -17,6 +17,7 @@ export const CUSTOM_TOKEN_ICONS: { [key: string]: boolean } = {
   'eth (portal)': true,
   eurc: true,
   gecko: true,
+  gofx: true,
   guac: true,
   hnt: true,
   jitosol: true,
